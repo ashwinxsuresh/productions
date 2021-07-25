7754c5b25f770485cb1bbfac29d982b34067b923
Ashwin_Suresh1 <Ashwin_Suresh1@Dell.com>
2021-07-25 13:21:09 -0500

    new update

---
+++ assets/js/content.js
@@ -0,0 +1,25 @@
+// const image_list = document.querySelector(".image_list");
+
+// let content = [
+//     {
+//         beg: '<li><a class="ns-img" href="images/',
+//         images: [
+//         "pic01.jpg",
+//         "pic02.jpg",
+//         "pic03.jpg",
+//         "pic04.jpg"
+//         ],
+//         end: '"></a></li>'
+//   },
+// ];
+
+// let image_tag = "";
+
+// var index = 0;
+
+// for(i=0; i < content[index].images.length; i++){
+//     image_tag += content[index].beg + content[index].images[i] + content[index].end + "\n";
+//     // alert(image_tag);
+// }
+
+// image_list.innerHTML = image_tag; //adding new div tag inside image_list
